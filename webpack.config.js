5b76e2896ad20bda63a1036b96c1276c8a391c66
Ryan Weaver <ryan@thatsquality.com>
2018-11-28 12:15:09 -0500

    adding the setup

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: {
+        entry0: './entry0',
+        //entry1: './entry1',
+        entry2: './entry2'
+    },
+    mode: 'production',
+    optimization: {
+        runtimeChunk: 'single'
+    }
+};
